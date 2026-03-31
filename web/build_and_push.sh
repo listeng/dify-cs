04d8a52af3431dcf3cf022038ddb5aab8f664d99
tls <tls@macbookpro-work.local>
2026-03-31 16:34:28 +0800

    chore(web): update build script for base path

--- web/build_and_push.sh
+++ web/build_and_push.sh
@@ -1,10 +1,11 @@
 export dd=$(date '+%Y%m%d%H%M')
+export NEXT_PUBLIC_BASE_PATH="${NEXT_PUBLIC_BASE_PATH:-/dnrai}"
 
-docker build -t hub.gxdnr.local/langgenius/dify-web:1.13.3-$dd .
+docker build --build-arg NEXT_PUBLIC_BASE_PATH="$NEXT_PUBLIC_BASE_PATH" -t hub.gxdnr.local/langgenius/dify-web:1.13.3-$dd .
 docker tag hub.gxdnr.local/langgenius/dify-web:1.13.3-$dd hub.gxdnr.local/langgenius/dify-web:latest
 
-# docker push hub.gxdnr.local/langgenius/dify-web:1.13.3-$dd
-# docker push hub.gxdnr.local/langgenius/dify-web:latest
+docker push hub.gxdnr.local/langgenius/dify-web:1.13.3-$dd
+docker push hub.gxdnr.local/langgenius/dify-web:latest
 
-docker save -o dify.tar hub.gxdnr.local/langgenius/dify-web:1.13.3-$dd
-crane push dify.tar hub.gxdnr.local/langgenius/dify-web:1.13.3-$dd
+#docker save -o dify.tar hub.gxdnr.local/langgenius/dify-web:1.13.3-$dd
+#crane push dify.tar hub.gxdnr.local/langgenius/dify-web:1.13.3-$dd
